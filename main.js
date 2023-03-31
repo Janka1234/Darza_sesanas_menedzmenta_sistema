40ebfa87e6f12f57fb9c5feb78c0f00576bba1b5
Janka1234 <90906121+Janka1234@users.noreply.github.com>
2023-03-31 12:32:04 +0300

    pievienoju  v airakas funkcijas

--- main.js
+++ main.js
@@ -6,36 +6,78 @@ const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 
 class Darzeni {
-    darzenis;
-    vaguSkaits;
-    vaguGarums;
+  darzenis;
+  vaguSkaits;
+  vaguGarums;
+  sekluSkaits;
+  atstarpe;
 
-    constructor(darzenis, vaguSkaits, vaguGarums) {
-        this.darzenis = darzenis;
-        this.vaguSkaits = vaguSkaits;
-        this.vaguGarums = vaguGarums;
-    }
+  constructor(darzenis, vaguSkaits, vaguGarums, sekluSkaits, atstarpe) {
+    this.darzenis = darzenis;
+    this.vaguSkaits = vaguSkaits;
+    this.vaguGarums = vaguGarums;
+    this.sekluSkaits = sekluSkaits;
+    this.atstarpe = atstarpe;
+  }
 
-    sekluDauduzms() {
-        
-    }
-    grabValues() {
-        
-    }
-    upsert() {
-        
-    }
-    
+  sekluDaudzums() {
+    return (this.kopGarums() / (parseInt(this.atstarpe)/100) - 2);
+  }
+  kopGarums() {
+    return (parseInt(this.vaguGarums) * parseInt(this.vaguSkaits));
+  }
+  upsert() {}
 }
 
 var btn = document.getElementById("btn-launch");
 var select = document.getElementById("select");
-var input = document.getElementById("vaguSkaits");
+var vSkaits = document.getElementById("vaguSkaits");
+var vGarums = document.getElementById("vaguGarums");
 var ul = document.getElementById("ul");
 
-btn.addEventListener("click", a);
-async function a() {
+btn.addEventListener("click", check);
+
+function check() {
+    console.log(select.value);
+    if (select.value == "" || vGarums.value== "" || vSkaits.value == "") {
+        alert("Lūdzu ievadiet / izvēlieties vērtības!");
+        return;
+    }
+    else {
+        vegetables();
+    }
+}
+
+async function vegetables() {
+    console.log(select);
     const { data, error } = await _supabase
-        .from("Registrs")
-        .select();
+        .from("Seklas")
+        .select("intervals")
+        .eq("darzenis", select.value)
+    console.log(data);
+
+
+    var veggies = new Darzeni(
+        select.value,
+        vSkaits.value,
+        vGarums.value,
+        5, //sekludaudzums,
+        data[0].intervals 
+    );
+
+    console.log(veggies.sekluDaudzums());
+
+    addList(veggies);
+}
+function addList(veggies) {
+    var li = document.createElement("li");
+    li.classList.add = ("list-group-item");
+    var text = document.createElement("h4");
+
+    text.innerText = ("Dārzenis = " + select.value +
+        ", Stādīšanas intervāls = " + veggies.atstarpe +
+        ", Nepieciešamās sēklas = " + veggies.sekluDaudzums());
+    
+    li.appendChild(text);
+    ul.appendChild(li);
 }
